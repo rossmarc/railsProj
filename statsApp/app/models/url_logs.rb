60771e361f6a482b5a06b7013c6e7a653bcbd97e
Marc Rossi <marcrossi@Marcs-MacBook-Pro.local>
2013-10-28 07:52:59 +0100

    after saved serialized

--- statsApp/app/models/url_logs.rb
+++ statsApp/app/models/url_logs.rb
@@ -1,3 +1,10 @@
 class UrlLogs < Sequel::Model
   
+  after_save :update_hash
+    
+  def update_hash
+    hash = Digest::MD5.hexdigest(self.id.to_s() + self.url + self.referrer + self.created_at.to_s())
+    self.update_column(:serialized, hash)
+  end
+  
 end
