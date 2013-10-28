6523d1c1cd98aa1d5eafae712293f3f16f8f5277
Marc Rossi <rossmarc@gmail.com>
2013-10-28 21:58:27 +0100

    test top referrers

--- statsApp/app/controllers/stats_controller.rb
+++ statsApp/app/controllers/stats_controller.rb
@@ -9,23 +9,36 @@ class StatsController < ApplicationController
   end
 
   def topReferrers
-   #  @viewsUrl = UrlLogs.group_and_count(:created_at, :url)
-#                   .where("created_at > ?", 5.days.ago)
-#                   .order(Sequel.desc(:count))
-#                   .limit(10)
-#                   
-#     @viewsUrl.each do |log|
-#     	
-#     	@topRefs = UrlLogs.group_and_count(:referrer)
-#     	  			.where("created_at = ?", log.created_at)
-#     	  			.where("url = ?", log.url)
-#     	  			.order(Sequel.desc(:count))
-#     	  			.limit(5)
-#     	
-#     end
-#     
-#                   
-#     render :json => @viewsUrl
+    viewsUrl = UrlLogs.group_and_count(:created_at, :url)
+                  .where("created_at > ?", 5.days.ago)
+                  .order(Sequel.desc(:count))
+                  .limit(10)
+                  
+    data = viewsUrl.map do |log|
+	
+    	topRefs = UrlLogs.select(:url).group_and_count(:referrer)
+    	  			.where("created_at = ?", log.created_at)
+    	  			.where("url = ?", log.url)
+    	  			.order(Sequel.desc(:count))
+    	  			.limit(5)
+
+    	referrers = topRefs.map do |ref|
+			var = {:url => ref.url, :visits => ref[:count]}
+			puts var
+		end
+		puts referrers
+		{log.created_at => 
+				[{
+					:url => log.url,
+					:visits => log[:count],
+					:referrer =>
+					 [referrers]
+				}]
+			}
+		
+    end
+                  
+    render :json => data.to_json
   end
   
   private
