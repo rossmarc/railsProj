f2d886318a5b0f0e764b12b8425cf4bde4e5711c
Marc Rossi <marcrossi@Marcs-MacBook-Pro.local>
2013-10-28 07:49:33 +0100

    first commit

--- stats/app/models/url_stats.rb
+++
@@ -1,9 +0,0 @@
-class UrlStats < ActiveRecord::Base
-    
-    after_save :update_hash
-    
-    def update_hash
-        hash = Digest::MD5.hexdigest(self.id.to_s() + self.url + self.referrer + self.created_at.to_s())
-        self.update_column(:serialized, hash)
-    end
-end
