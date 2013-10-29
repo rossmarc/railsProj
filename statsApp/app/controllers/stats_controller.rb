6da6a41802411ab102b52612416e84c25822f0e1
Marc Rossi <rossmarc@gmail.com>
2013-10-29 22:41:12 +0100

    final version to test

--- statsApp/app/controllers/stats_controller.rb
+++ statsApp/app/controllers/stats_controller.rb
@@ -5,37 +5,43 @@ class StatsController < ApplicationController
   
   def topUrls
     viewsUrl = UrlLogs.group_and_count(:created_at, :url).where("created_at >= ?", 5.days.ago).order(Sequel.desc(:created_at))
-    data = viewsUrl.inject({}) do |h,log| 
+    h = Hash.new() 
+    viewsUrl.each do |log| 
     	h[log.created_at] ||= [] #define if nil
     	h[log.created_at] << {:url => log.url, :visits => log[:count]} #append new value
-    	h # object we are building
     end
-    render :json => data.to_json
+    render :json => h.to_json
   end
 
   def topReferrers
-    viewsUrl = UrlLogs.group_and_count(:created_at, :url)
-                  .where("created_at >= ?", 5.days.ago)
-                  .having(:count 
-                  .order(Sequel.desc(:count))
-	h = Hash.new() 
-	@ref = Hash.new()   
+  	h = Hash.new() 
+	query = "SELECT `created_at`, `url`, count(*) AS `count` FROM `url_logs` 
+			 WHERE (created_at >= '"+5.days.ago.to_s+"')
+			 GROUP BY `created_at`, `url` ORDER BY `created_at` DESC, `count` DESC"
+				
+	viewsUrl = UrlLogs.db().fetch("
+			SELECT * FROM ("+query+") AS t1
+			where (
+	   		SELECT COUNT(*) FROM ("+query+") as u
+			where u.created_at = t1.created_at and u.url <= t1.url
+			) <= 4;
+		")
+	
     viewsUrl.each do |log| 
-    
     	topRefs = UrlLogs.group_and_count(:referrer)
-    	  			.where("created_at = ?", log.created_at)
-    	  			.where("url = ?", log.url)
+    	  			.where("created_at = ?", log[:created_at])
+    	  			.where("url = ?", log[:url])
     	  			.order(Sequel.desc(:count))
     	  			.limit(5)
-
+    	
+    	@ref = Hash.new()	
 		topRefs.each do |ref|
-			@ref[log.url] ||= []
-			@ref[log.url] << {:url => ref.referrer, :visits => ref[:count]}
+			@ref[log[:url]] ||= []
+			@ref[log[:url]] << {:url => ref.referrer, :visits => ref[:count]}
  		end
 
- 		h[log.created_at] ||= [] #define if nil
-    	h[log.created_at] << {:url => log.url,:visits => log[:count],:referrer => @ref[log.url]} #append new value
-
+ 		h[log[:created_at]] ||= [] #define if nil
+    	h[log[:created_at]] << {:url => log[:url],:visits => log[:count],:referrer => @ref[log[:url]]} #append new value
       end     
     render :json => h.to_json
   end
