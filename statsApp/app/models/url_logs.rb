1fe71cb8af4f288cefedf483fe6ce3733f2f75dc
Marc Rossi <rossmarc@gmail.com>
2013-10-30 21:33:36 +0100

    hash create

--- statsApp/app/models/url_logs.rb
+++ statsApp/app/models/url_logs.rb
@@ -2,7 +2,11 @@ class UrlLogs < Sequel::Model
       
   def after_create
   	super
-    hash = Digest::MD5.hexdigest(self.id.to_s() + self.url + self.referrer + self.created_at.to_s())
+  	if (self.referrer != 'NULL') 
+    	hash = Digest::MD5.hexdigest(self.id.to_s() + self.url + self.referrer + self.created_at.to_s())
+    else
+    	hash = Digest::MD5.hexdigest(self.id.to_s() + self.url + self.created_at.to_s())
+    end
     self.set(:serialized => hash)
     self.save
   end
